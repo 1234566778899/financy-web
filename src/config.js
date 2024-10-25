58a310b8f5b7fdc33cc264643117f2741cf33e8d
Carlos Jesús Ordaz Hoyos <carlos.ordaz@acomo.com.pe>
2024-10-25 17:58:46 -0500

    xx

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    //uri: 'https://financy-ggb7chgda2chaxg6.eastus-01.azurewebsites.net/'
-    uri: 'http://localhost:4000'
+    uri: 'https://financy-ggb7chgda2chaxg6.eastus-01.azurewebsites.net/'
+    //uri: 'http://localhost:4000'
 }
